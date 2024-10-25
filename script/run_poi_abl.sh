24a730abcab8c3cfa24ad30b34e1bf39dd7d9b8a
zhangwentao <zhangwentao@bigai.ai>
2024-10-25 17:24:40 +0800

    init

--- script/run_poi_abl.sh
+++
@@ -1,5 +0,0 @@
-# nohup sh script/run_poi_abl.sh > log/run_poi_abl.log 2>&1 &
-# python run_model.py --task poi_representation --model CTLE --dataset nyc  --gpu_id 0 --config poi_config_128 --exp_id CTLEM --abl mh
-python run_model.py --task poi_representation --model CTLE --dataset nyc  --gpu_id 0 --config poi_config_128 --exp_id CTLEL --abl lstm --train false 
-# python run_model.py --task poi_representation --model CTLE --dataset nyc  --gpu_id 0 --config poi_config_128 --exp_id CTLEC --abl contr
-# python run_model.py --task poi_representation --model CACSR --dataset nyc  --gpu_id 0 --config poi_config_128 --exp_id CTLET --abl te
--- script/run_road_abl0.sh
+++
@@ -1,4 +0,0 @@
-# nohup sh script/run_road_abl0.sh > log/run_road_abl0.log 2>&1 &
-python run_model.py --task road_representation --model JCLRNT --dataset xa  --gpu_id 2 --config road_config_128 --exp_id JCLRNTRI --abl recon\&infer --train false
-python run_model.py --task road_representation --model JCLRNT --dataset xa  --gpu_id 2 --config road_config_128 --exp_id JCLRNTR --abl recon --train false
-python run_model.py --task road_representation --model JCLRNT --dataset xa  --gpu_id 2 --config road_config_128 --exp_id JCLRNTI --abl infer --train false
